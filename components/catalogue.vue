595d4cf54db06c44865c74a271f3b940b25235fe
majordomo-systems <admin@majordomo.consulting>
2021-03-06 16:09:41 -0500

    Alignment

--- components/catalogue.vue
+++ components/catalogue.vue
@@ -6,19 +6,19 @@
       <div class="section-heading pl-6">
         <h2>Category A</h2>
       </div>
-        <hr />
-        <ul>
-            <li v-for="product in this.$store.state.products" :key="product.id">
-        <div class="columns is-centered is-vcentered">
+      <hr />
+      <ul>
+        <li v-for="product in this.$store.state.products" :key="product.id">
+          <div class="columns is-vcentered">
             <div class="column is-one-quarter"></div>
-            <div class="column pl-4">
-            <div class="product">
-                <img class="image-rounded" :src="product.image" :alt="product.title">
-                <div class="has-text-weight-bold">{{ product.title }}</div>
-                <div class="product-description">{{ product.description }}</div>
+            <div class="column product-align">
+              <div>
+                  <img class="image-rounded" :src="product.image" :alt="product.title">
+                  <div class="has-text-weight-bold">{{ product.title }}</div>
+                  <div class="product-description">{{ product.description }}</div>
+              </div>
             </div>
-            </div>
-            <div class="column pl-4">
+            <div class="column product-align">
             <div class="is-size-2 has-text-success">${{ product.price }}</div>
             <b-button class="snipcart-add-item"
                 data-item-url="/products/"
@@ -30,40 +30,40 @@
                 Add to cart
             </b-button>
             </div>
-        </div>
+          </div>
         </li>
-        </ul>
-        <div class="section-heading pl-6 pt-6">
-          <h2>Category B</h2>
-        </div>
-        <hr />
-        <ul>
-            <li v-for="product in this.$store.state.moreproducts" :key="product.id">
-        <div class="columns is-centered is-vcentered">
-            <div class="column is-one-quarter"></div>
-            <div class="column pl-4">
-            <div class="product">
-                <img class="image-rounded" :src="product.image" :alt="product.title">
-                <div class="has-text-weight-bold">{{ product.title }}</div>
-                <div class="product-description">{{ product.description }}</div>
-            </div>
-            </div>
-            <div class="column pl-4">
-            <div class="is-size-2 has-text-success">${{ product.price }}</div>
-            <b-button class="snipcart-add-item"
-                data-item-url="/products/"
-                :data-item-id="product.id"
-                :data-item-image="product.image"
-                :data-item-name="product.title"
-                :data-item-price="product.price"
-                >
-                Add to cart
-            </b-button>
-            </div>
-        </div>
+      </ul>
+      <div class="section-heading pl-6 pt-6">
+        <h2>Category B</h2>
+      </div>
+      <hr />
+      <ul>
+        <li v-for="product in this.$store.state.moreproducts" :key="product.id">
+          <div class="columns is-vcentered">
+              <div class="column is-one-quarter"></div>
+              <div class="column product-align">
+                <div class="product">
+                    <img class="image-rounded" :src="product.image" :alt="product.title">
+                    <div class="has-text-weight-bold">{{ product.title }}</div>
+                    <div class="product-description">{{ product.description }}</div>
+                </div>
+              </div>
+              <div class="column product-align">
+              <div class="is-size-2 has-text-success">${{ product.price }}</div>
+              <b-button class="snipcart-add-item"
+                  data-item-url="/products/"
+                  :data-item-id="product.id"
+                  :data-item-image="product.image"
+                  :data-item-name="product.title"
+                  :data-item-price="product.price"
+                  >
+                  Add to cart
+              </b-button>
+              </div>
+          </div>
         </li>
-        </ul>
-    </div>
+      </ul>
+  </div>
 </template>
 
 <style>
